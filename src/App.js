6fa6221893819f1712f4635622c823afdfc07409
Yugank Singh <badbudarsingh@gmail.com>
2021-05-06 17:56:20 +0530

    add increase and decrease quantity

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import CartItem from './CartItem'
+import './index.css'; 
 
 function App() {
   return (
--- src/CartItem.js
+++ src/CartItem.js
@@ -1,18 +1,58 @@
 import React from 'react'
 
 class CartItem extends React.Component {
+	constructor(){
+		super()
+		this.state = {
+			price: 99999999,
+			title: "Bey Em Way",
+			qty: 369,
+			img: '',
+		}
+	}
+	increaseQuantity = () => {
+		this.state.qty++
+		console.log('this', this.state)
+		this.setState({qty: this.state.qty})
+	}
+	decreaseQuantity = () => {
+		this.state.qty--
+		console.log('this', this.state)
+		this.setState({qty: this.state.qty})
+	}
+
+
+
 	render () {
+		const { price, title , qty } = this.state
 		return (
 			<div className="cart-item">
 				<div className="left-block">
-					<img src="" style={styles.image} /> 
+					<img alt="product-image" src="" style={styles.image} /> 
 				</div>
 				<div className="right-block">
-					<div style={ { fontSize: '1.56em' } } >Phone</div>
-					<div style={ { color: '#777' } } >Rs 999</div>
-					<div style={ { color: '#777' } } >Qty: 1</div>
+					<div style={ { fontSize: '1.56em' } } >{ title }</div>
+					<div style={ { color: '#777' } } >Rs { price }</div>
+					<div style={ { color: '#777' } } >Qty: { qty }</div>
 					<div className="cart-item-actions">
-						{/* Buttons */}
+						<img 
+							alt="increase" 
+							className="action-icons" 
+							src="https://image.flaticon.com/icons/png/128/1237/1237946.png"
+							onClick={ this.increaseQuantity }
+						/>
+						<img 
+							alt="decrease" 
+							className="action-icons" 
+							src="https://t4.ftcdn.net/jpg/03/16/36/03/240_F_316360373_uWcj5rZxsUbmoAogMfow8EZhUOn7FTM0.jpg"
+							onClick={ this.decreaseQuantity }
+						/>
+						<img 
+							alt="delete" 
+							className="action-icons" 
+							src="https://t4.ftcdn.net/jpg/00/98/26/11/240_F_98261175_Sv69O3rZsHApYkjAdrWbgQixYHwyZyOr.jpg"
+							onClick={ this.deleteItem }
+						/>
 					</div>
 				</div>
 			</div>
